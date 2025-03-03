d68947c7b370d3f137811af12275d6f3ee46abfd
DakshKumarShah <daksh.kumar@bcah.christuniversity.in>
2025-03-03 20:36:52 +0530

    Update population.js

--- IndiaProject/js/population.js
+++ IndiaProject/js/population.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const state = document.body.getAttribute('data-state');
-    fetch('https://run.mocky.io/v3/3945b68e-bdb6-4d0a-9900-013889736976')
+    fetch('579b464db66ec23bdd000001f93dc20faa91454e728a0258f629da89')
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
